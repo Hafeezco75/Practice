d97512c30c8abb39d8fadb394e606074989a7290
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2024-09-25 10:18:06 +0100

    sender

---
+++ src/test/java/com/sevenStars/HotelSystem/HotelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenStars.HotelSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
